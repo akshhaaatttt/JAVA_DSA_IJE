283c00e7c72c3d9bdda72b69f5010b637418a879
akshhaaatttt <akshatjain1615@gmail.com>
2025-07-08 20:00:18 +0530

    intellij repo

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My Name is Akshat ");
+        System.out.println("How we take input in java");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+    }
+}
