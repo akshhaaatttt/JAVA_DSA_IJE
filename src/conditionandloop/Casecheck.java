5b190aa78a036504fa5362f5f1649580302b2a24
akshhaaatttt <akshatjain1615@gmail.com>
2025-07-12 22:17:32 +0530

    Day 4 of DSA

---
+++ src/conditionandloop/Casecheck.java
@@ -0,0 +1,16 @@
+package conditionandloop;
+
+import java.util.Scanner;
+
+public class Casecheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().trim().charAt(0);
+        if(ch >= 'a' && ch <= 'z'){
+            System.out.println("Lower Case");
+        } else {
+            System.out.println("UpperCase");
+        }
+//        System.out.println(ch);
+    }
+}
