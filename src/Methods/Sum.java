3f25c5c57c1fee64bde2a7fec58369422f9e3a6e
akshhaaatttt <akshatjain1615@gmail.com>
2025-07-15 23:11:14 +0530

    more methods

--- src/Methods/Sum.java
+++ src/Methods/Sum.java
@@ -4,8 +4,30 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+//        sum();  //function call
+//        int ans = sum2();
+//        System.out.println(ans);
 
+        int ans1 = sum3(20, 40);
+        System.out.println(ans1);
+
+
+    }
+
+    static int sum3(int a, int b){
+        int add1 = a+b;
+        return add1;
+    }
+
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = in.nextInt();
+        System.out.println("Enter the second number");
+        int num2 = in.nextInt();
+        int add = num1 + num2;
+        System.out.print("The sum of two numbers is: "+ add);
+        return add;
     }
 
     static void sum(){
