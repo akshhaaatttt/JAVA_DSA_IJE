f0a769c6595c2637019e3eed2d591c7f2c96e104
akshhaaatttt <akshatjain1615@gmail.com>
2025-07-16 18:22:25 +0530

    Swap

--- src/Methods/Greeting.java
+++ src/Methods/Greeting.java
@@ -1,10 +1,22 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
-        greeting();
+//        greeting();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = in.next();
+        String personalised = myGreet(name);
+        System.out.println(personalised);
     }
-    static void greeting(){
-        System.out.println("Hello World");
+    static String myGreet(String name) {
+        String message = "Hello " + name;
+        return message;
     }
+
+//    static void greeting(){
+//        System.out.println("Hello World");
+//    }
 }
