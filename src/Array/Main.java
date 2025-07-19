271eac4e770c16278a7b234291ea9aa84b1b4f30
akshhaaatttt <akshatjain1615@gmail.com>
2025-07-19 23:47:49 +0530

    Array

---
+++ src/Array/Main.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        // Q store a roll number
+        int a  = 19;
+
+        //Q store 5 roll numbers
+        // so we use array
+        // Syntax
+
+        // Datatype [] variable_name = new datatype[size]
+        int[] rnos = new int[5];
+
+        // or directly
+        int[] rnos2 = {23,12,45,34};
+    }
+}
