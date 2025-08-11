8a5493e3da0291bd6708b34b27915eb94b0b6a38
akshhaaatttt <akshatjain1615@gmail.com>
2025-08-12 00:04:33 +0530

    long time

---
+++ src/Array/ArrayListExample.java
@@ -0,0 +1,39 @@
+package Array;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Syntax
+        ArrayList<Integer> list = new ArrayList<>(10);
+//        list.add(67);
+//        list.add(40);
+//        list.add(25);
+//        list.add(27);
+//        list.add(135);
+//        list.add(97);
+//        list.add(445);
+
+
+//        System.out.println(list.contains(135));
+//        System.out.println(list);
+//        list.set(0,99);
+//        list.remove(2);
+//        System.out.println(list);
+
+
+        for(int i=0; i<5; i++){
+            list.add(in.nextInt());
+        }
+
+        for(int i = 0; i<5; i++){
+            System.out.println(list.get(i)); // pass index here, list[index] syntax will not work here
+        }
+
+        System.out.println(list);
+
+    }
+}
