bfb6a6b77c8d68c045e577d30aae2fa85488d0cb
akshhaaatttt <akshatjain1615@gmail.com>
2025-09-05 19:54:33 +0530

    Binary sort completed

--- src/RowColMatrix/SortedMatrix.java
+++ src/RowColMatrix/SortedMatrix.java
@@ -1,8 +1,15 @@
 package RowColMatrix;
 
+import java.util.Arrays;
+
 public class SortedMatrix {
     public static void main(String[] args) {
-
+        int[][] arr ={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println(Arrays.toString(search(arr,6)));
     }
     // Search in the rows provided and the columns provided
     static int[] binarySearch(int[][] matrix, int row, int cStart, int cEnd, int target){
@@ -14,7 +21,7 @@ public class SortedMatrix {
             if(matrix[row][mid] < target){
                 cStart = mid+1;
             } else {
-                cEnd = mid-1
+                cEnd = mid-1;
             }
         }
         return new int[]{-1,-1};
@@ -49,9 +56,23 @@ public class SortedMatrix {
         if(matrix[rStart][cMid] == target){
             return new int[]{rStart+1,cMid};
         }
+
         //search in 1st half
+        if(target <= matrix[rStart][cMid-1]){
+            return binarySearch(matrix,rStart,0,cMid-1,target);
+        }
         //search in 2nd half
+        if(target >= matrix[rStart][cMid+1] && target <= matrix[rStart][cols-1]){
+            return binarySearch(matrix,rStart,cMid+1,cols-1,target);
+        }
         //search in 3rd half
+        if(target <= matrix[rStart+1][cMid-1]){
+            return binarySearch(matrix,rStart+1,0,cMid-1,target);
+        }
         //search in 4th half
+        else{
+            return binarySearch(matrix,rStart+1,cMid+1,cols-1,target);
+
+        }
     }
 }
