74f102d8a33a4f35a11f6b5e5dce996377d54f51
akshhaaatttt <akshatjain1615@gmail.com>
2025-09-08 22:10:40 +0530

    Cyclic Sort

---
+++ src/Sorting/CyclicSort/CycleSortAlgo.java
@@ -0,0 +1,28 @@
+package Sorting.CyclicSort;
+
+import java.util.Arrays;
+
+public class CycleSortAlgo {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicSort(int[] arr){
+        int i =0;
+        while(i < arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr, i,correct);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr,int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
